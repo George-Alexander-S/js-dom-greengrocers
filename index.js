0d6af22c418a6efd672acbf2214045170644dd0a
George <george.alexander.saav@gmail.com>
2024-09-26 13:55:12 +0200

    in progress

--- index.js
+++ index.js
@@ -53,3 +53,61 @@ const state = {
   ],
   cart: []
 };
+
+const store = document.querySelector("#store-list");
+// const store = document.querySelector("item-list store--item-list")
+function storeitems() {
+  // store.innerHTML = '';
+
+  state.items.map((s) => {
+    console.log(s)
+    const li = document.createElement("li")
+
+    const img = document.createElement("img")
+    let imgstring = `/assets/icons/${s.id}.svg`
+    img.src = imgstring
+    li.appendChild(img)
+
+    const addButton = document.createElement("button")
+    addButton.innerHTML = "Add to cart"
+    addButton.onclick = function () {
+      cartitems(s.id, s.name)
+    }
+
+    li.appendChild(addButton)
+
+    store.appendChild(li)
+  });
+}
+storeitems();
+
+const items = document.querySelector("#items-cart")
+
+function cartitems(id, name) {
+  const li = document.createElement("li")
+  const img = document.createElement("img")
+  let imgstring = `/assets/icons/${id}.svg`
+  img.src = imgstring
+  li.appendChild(img)
+
+  const p = document.createElement("p")
+  p.innerHTML = name
+  li.appendChild(p)
+
+  const reduce = document.createElement("button")
+  reduce.className="quantity-btn remove-btn center"
+  reduce.innerHTML="-"
+  li.appendChild(reduce)
+
+  const span = document.createElement("span")
+  span.className="quantity-text center"
+  span.innerHTML="1"
+  li.appendChild(span)
+
+  const add = document.createElement("button")
+  add.className="quantity-btn add-btn center"
+  add.innerHTML="+"
+  li.appendChild(add)
+
+  items.appendChild(li)
+}
